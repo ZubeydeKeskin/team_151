2a43dfe843ba7ae8e90903dead8b76d032177415
Hasan ARIKAN <hasannarikan@gmail.com>
2022-05-23 20:39:30 +0300

    Gerekli dosyalar için ekleme ve çıkarmalar

--- Assets/Scripts/Mechanics/FirstZone.cs
+++ Assets/Scripts/Mechanics/FirstZone.cs
@@ -10,18 +10,24 @@ using static Platformer.Core.Simulation;
 namespace Platformer.Mechanics
 {
     /// <summary>
-    /// FirstZone'u başlatır, birinci seviyeye geçer.
+    /// FirstZone'u başlatır, birinci seviyeye geçer. + Baslangic ekranını başlatır
     /// </summary>
     
     public class FirstZone : MonoBehaviour
     {
         //public TextMeshProUGUI bolum1Score;
-        void OnTriggerStay2D(Collider2D collider)
+        private void OnTriggerStay2D(Collider2D collider)
         {
             if (Input.GetButton("Vertical"))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
+        }
+        
+        //Baslangic ekranında oyunu başlatmak için gerekli olan next() fonksiyonu
+        public void next()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
